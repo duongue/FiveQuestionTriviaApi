ff03ab08c1ab8383d0f312390c486e3b2d38c89d
Duong Pham <dpham@cougars.ccis.edu>
2018-01-07 15:08:42 -0600

    Added autofac for dependency injections

---
+++ Data/DataAutofacModule.cs
@@ -0,0 +1,25 @@
+﻿using Autofac;
+using Data.Repository;
+using Data.UnitOfWork;
+using Domain;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Data
+{
+    public class DataAutofacModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<UnitOfWorkImp>().As<IUnitOfWork>();
+
+            builder.RegisterType<RepositoryImp<Category>>().As <IRepository<Category>>();
+            builder.RegisterType<RepositoryImp<Level>>().As<IRepository<Level>>();
+            builder.RegisterType<RepositoryImp<Question>>().As<IRepository<Question>>();
+            builder.RegisterType<RepositoryImp<Encouragement>>().As<IRepository<Encouragement>>();
+
+            builder.RegisterType<FiveQuestionEntities>().AsSelf().InstancePerLifetimeScope();
+        }
+    }
+}
